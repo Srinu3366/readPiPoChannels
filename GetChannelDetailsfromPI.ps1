2a2c958865e8f39ab4736204c4670db3c7f36260
Reddy <Srinivasu.Reddy@chsinc.com>
2019-05-07 11:10:55 -0500

    added channel xmls download option

--- GetChannelDetailsfromPI.ps1
+++ GetChannelDetailsfromPI.ps1
@@ -14,11 +14,13 @@ $otherChannels = @()
 $properties = Get-Content .\Properties.json | ConvertFrom-Json
 
 $url = $properties.pi_system_url
-if( $null -ne $properties.file_name){
+if ( $null -ne $properties.file_name) {
     $outputpath = $properties.file_name
-}else{
+}
+else {
     $outputpath = $outputpath = join-path -Path $env:USERPROFILE -ChildPath "desktop\PIPOChannelDetails.xlsx"
 }
+$download_channel_xml = $properties.download_channel_xml
 
 [Xml] $SOAPRequest = Get-Content .\ChannelQueryRequest.xml
 [Xml] $SOAPReadChannelRequest = Get-Content .\ChannelReadRequest.xml
@@ -43,11 +45,20 @@ foreach ($channel in $ReturnXml.GetElementsByTagName('rn3:CommunicationChannelID
     $channelObjList += $channelObj
 }
 
+$channelXmlFilePath = '.\Channels' 
 Write-Host 'Reading Channel XML configuration.....'
 foreach ( $channel in $channelObjList ) {
     $request = [Xml] $SOAPReadChannelRequest.OuterXml.Replace("{{PartyID}}", $channel.PartyID).Replace("{{ComponentID}}", $channel.ComponentID).Replace("{{ChannelID}}", $channel.ChannelID)
 
     $ReturnXml = CallPIChannelService $request $authorization $url
+    if($download_channel_xml){
+        if($channel.PartyID){
+            $fileName = "{0}\{1}_{2}_{3}.{4}" -f $channelXmlFilePath, $channel.PartyID, $channel.ComponentID, $channel.ChannelID, 'xml'
+        } else {
+            $fileName = "{0}\{1}_{2}.{3}" -f $channelXmlFilePath, $channel.ComponentID, $channel.ChannelID, 'xml'
+        }
+        $ReturnXml.OuterXml | Out-File $fileName
+    }
     $direction = $ReturnXml.GetElementsByTagName('rn3:CommunicationChannel').Direction
 
     $adapterType = $ReturnXml.GetElementsByTagName('rn3:AdapterMetadata').name
@@ -97,44 +108,56 @@ $excel = New-Object -ComObject excel.application
 $excel.visible = $false
 $workbook = $excel.Workbooks.Add()
 
-#give the remaining worksheet a name
-$otherChannelsSheet = $workbook.Worksheets.Item(1)
-$otherChannelsSheet.name = 'Other Channels'
-$otherChannelsSheet = OtherAdapterChannelsToExcel $otherChannels $otherChannelsSheet
-#adjusting the column width so all data's properly visible
-$otherChannelsSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
+if ($otherChannels) {
+    #give the remaining worksheet a name
+    $otherChannelsSheet = $workbook.Worksheets.Item(1)
+    $otherChannelsSheet.name = 'Other Channels'
+    $otherChannelsSheet = OtherAdapterChannelsToExcel $otherChannels $otherChannelsSheet
+    #adjusting the column width so all data's properly visible
+    $otherChannelsSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
+}
 
-$idocChannelsSheet = $workbook.Worksheets.add()
-$idocChannelsSheet.name = 'Idoc Channels'
-$idocChannelsSheet = IdocChannelToExcel $idocChannels $idocChannelsSheet
-$idocChannelsSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
+if ($idocChannels) {
+    $idocChannelsSheet = $workbook.Worksheets.add()
+    $idocChannelsSheet.name = 'Idoc Channels'
+    $idocChannelsSheet = IdocChannelToExcel $idocChannels $idocChannelsSheet
+    $idocChannelsSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
+}
 
-$restChannelsSheet = $workbook.Worksheets.add()
-$restChannelsSheet.name = 'REST Channels'
-$restChannelsSheet = RestChannelToExcel $restChannels $restChannelsSheet
-$restChannelsSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
+if($restChannels){
+    $restChannelsSheet = $workbook.Worksheets.add()
+    $restChannelsSheet.name = 'REST Channels'
+    $restChannelsSheet = RestChannelToExcel $restChannels $restChannelsSheet
+    $restChannelsSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
+}
 
-$soapChannelsSheet = $workbook.Worksheets.add()
-$soapChannelsSheet.name = 'SOAP Channels'
-$soapChannelsSheet = SoapChannelToExcel $soapChannels $soapChannelsSheet
-$soapChannelsSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
+if($soapChannels){
+    $soapChannelsSheet = $workbook.Worksheets.add()
+    $soapChannelsSheet.name = 'SOAP Channels'
+    $soapChannelsSheet = SoapChannelToExcel $soapChannels $soapChannelsSheet
+    $soapChannelsSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
+}
 
-$sftpChannelSheet = $workbook.Worksheets.add()
-$sftpChannelSheet.name = 'SFTP Channels'
-$sftpChannelSheet = SftpChannelToExcel $sftpChannels $sftpChannelSheet
-$sftpChannelSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
+if($sftpChannels){
+    $sftpChannelSheet = $workbook.Worksheets.add()
+    $sftpChannelSheet.name = 'SFTP Channels'
+    $sftpChannelSheet = SftpChannelToExcel $sftpChannels $sftpChannelSheet
+    $sftpChannelSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
+}
 
-$fileChannelSheet = $workbook.Worksheets.add()
-$fileChannelSheet.Name = 'File Channels'
-$fileChannelSheet = FileChannelToExcel $fileChannels $fileChannelSheet
-$fileChannelSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
+if($fileChannels){
+    $fileChannelSheet = $workbook.Worksheets.add()
+    $fileChannelSheet.Name = 'File Channels'
+    $fileChannelSheet = FileChannelToExcel $fileChannels $fileChannelSheet
+    $fileChannelSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
+}
 
-
-$jdbcChannelSheet = $workbook.Worksheets.add()
-$jdbcChannelSheet.name = 'JDBC Channels'
-$jdbcChannelSheet = JdbcChannelToExcel $jdbcChannels $jdbcChannelSheet
-$jdbcChannelSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
-
+if($jdbcChannels){
+    $jdbcChannelSheet = $workbook.Worksheets.add()
+    $jdbcChannelSheet.name = 'JDBC Channels'
+    $jdbcChannelSheet = JdbcChannelToExcel $jdbcChannels $jdbcChannelSheet
+    $jdbcChannelSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
+}
 
 #saving & closing the file
 $workbook.SaveAs($outputpath)
